eff830816116ee845d96828dccd46c64e4b0f697
Panxiandiao <1045366459@qq.com>
2019-06-14 22:07:19 +0800

    按错了

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,17 +1,16 @@
-// import originJSONP from 'jsonp'
+import originJSONP from 'jsonp'
 
- function jsonp(url, data, option) {
+export default function jsonp(url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
-  console.log(url)
-  // return new Promise((resolve, reject) => {
-  //   originJSONP(url, option, (err, data) => {
-  //     if (!err) {
-  //       resolve(data)
-  //     } else {
-  //       reject(err)
-  //     }
-  //   })
-  // })
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
 }
 function param(data) {
   let url = ''
@@ -21,10 +20,3 @@ function param(data) {
   }
   return url ? url.substring(1) : ''
 }
-
-const url = 'https://www.baidu.com'
-const data = {
-  time: 155,
-  name: 'patrick'
-}
-jsonp(url, data)
