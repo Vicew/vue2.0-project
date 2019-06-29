bcc25daeaeffb491682ba603bcadf026b5e7792c
Panxiandiao <1045366459@qq.com>
2019-06-29 16:59:21 +0800

    懒加载图片显示不了问题

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
 import '@/common/stylus/index.styl'
+import VueLazyload from 'vue-lazyload'
+
+Vue.use(VueLazyload)
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/wait.png')
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
