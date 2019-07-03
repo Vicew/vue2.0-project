79ab2ff9247ccf275309a02274c15bd09e91453e
Panxiandiao <1045366459@qq.com>
2019-07-03 23:35:31 +0800

    初试vuex

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -11,6 +11,7 @@ import {getSingerList} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import Singer from 'common/js/singer'
 import ListView from 'base/listview/listview'
+import {mapMutations} from 'vuex'
 
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
@@ -80,7 +81,10 @@ export default{
         return a.title.charCodeAt(0) - b.title.charCodeAt(0)
       })
       return hot.concat(ret)
-    }
+    },
+    ...mapMutations({
+      setSinger: 'SET_SINGER'
+    })
   },
   components: {
     ListView
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as actions from './action'
+import * as actions from './actions'
 import * as getters from './getters'
 import state from './state'
 import mutations from './mutations'
