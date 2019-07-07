f4fd8e8ad9d3cb0da8f45300bdc68354e3d318ff
Panxiandiao <1045366459@qq.com>
2019-07-07 10:57:32 +0800

    播放器vuex配制

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false, // 播放暂停状态
+  fullScreen: false, // 播放器展开和收起状态
+  playlist: [], // 播放列表
+  sequenceList: [], // 歌曲列表（顺序的）
+  mode: playMode.sequence, // 三种播放模式
+  currentIndex: -1 // 当前播放的哪首歌的索引
 }
 
 export default state
